94af0b9c3d37f326d0210d9ea13bb14d7e832c1f
BugHunter-X <askzenai@gmail.com>
2025-01-02 06:30:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using the Expo Camera API with certain Android devices. The camera preview fails to render, resulting in a blank screen.  The error is not consistently reproducible across all devices or Expo versions. There are no explicit error messages in the console.
